5fd96975a5b47afc3929afa0294580a79c0f85b0
Rafael Santos <rafa.05125@gmail.com>
2022-05-23 15:12:48 -0300

    POST method implemented in order to create a new account

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const { v4 } = require("uuid")
 
 const app = express();
+app.use(express.json());
+const users = [];
+/*
+* cpf
+* name
+* id
+* proofOfResidence
+* proofOfIncome 
+* statement[]
+*/
 
-app.get("/test", (request,response) => {
-    return response.send({message: "Hello World - Test"});
+app.post("/account", (request, response) => {
+    const { cpf, name, proofOfResidence, proofOfIncome } = request.body;
+    const userCpfValidator = users.some((user) => user.cpf === cpf);
+    
+    if(userCpfValidator){
+        return response.status(400).json({error: "This user already exists. Please try another account or contact the support."})
+    }
+
+    users.push({
+        cpf,
+        name,
+        id: v4(),
+        proofOfResidence,
+        proofOfIncome,
+        statement: []
+    });
+
+    return response.status(201).send();
 });
 
 app.listen(3333);
